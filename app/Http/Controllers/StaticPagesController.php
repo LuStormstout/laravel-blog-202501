3b298ecad175ad3554fe9288b2825a79c2e9c216
Lu Stormstout <lustormstout@gmail.com>
2025-02-12 22:17:07 +0900

    给 StaticPagesController 文件中的方法添加注释信息

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -9,16 +9,31 @@ use Illuminate\Http\Request;
 
 class StaticPagesController extends Controller
 {
+    /**
+     * Show the home page.
+     *
+     * @return Application|Factory|View
+     */
     public function home(): Factory|View|Application
     {
         return view('static_pages/home');
     }
 
+    /**
+     * Show the help page.
+     *
+     * @return Application|Factory|View
+     */
     public function help(): Factory|View|Application
     {
         return view('static_pages/help');
     }
 
+    /**
+     * Show the about page.
+     *
+     * @return Application|Factory|View
+     */
     public function about(): Factory|View|Application
     {
         return view('static_pages/about');
