1446e92e31e8f49a8e6bf59f3256e98a1342381c
Lu Stormstout <lustormstout@gmail.com>
2025-02-12 22:07:08 +0900

    命名路由

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -3,6 +3,6 @@
     <a href="https://laravel.com" target="_blank">Laravel</a>
 
     <div class="float-end">
-        <a href="/about" class="text-decoration-none">关于</a>
+        <a href="{{ route('about') }}" class="text-decoration-none">关于</a>
     </div>
 </footer>
