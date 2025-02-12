378c29e72beb34c52c70c95d8a7855ea9e366855
Lu Stormstout <lustormstout@gmail.com>
2025-02-12 21:40:07 +0900

    静态文件浏览器缓存问题

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,7 +2,7 @@
 <html lang="zh-CN">
 <head>
     <title>@yield('title', 'Lu Stormstout')</title>
-    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
 
