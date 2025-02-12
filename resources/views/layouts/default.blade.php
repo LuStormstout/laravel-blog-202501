b050e8e9153d00143d470d943cb8db97cf495f9e
Lu Stormstout <lustormstout@gmail.com>
2025-02-12 21:19:40 +0900

    初始化样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,23 @@
 <!DOCTYPE html>
 <html lang="zh-CN">
 <head>
-    <title>@yield('title', 'Blog')</title>
+    <title>@yield('title', 'Lu Stormstout')</title>
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-@yield('content')
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <a class="navbar-brand" href="/">Lu Stormstout</a>
+        <ul class="navbar-nav justify-content-end">
+            <li class="nav-item"><a class="nav-link" href="/help">Help</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">Login</a></li>
+        </ul>
+    </div>
+</nav>
+
+<div class="container">
+    @yield('content')
+</div>
 </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.default')
 
 @section('content')
-    <h1>主页</h1>
+    <div class="bg-light p-3 p-sm-5 rounded">
+        <h1>Lu Stormstout</h1>
+        <p class="lead">
+            Hi, I'm Lu Stormstout, a web developer from Pandaria.
+        </p>
+        <p>
+            This is the first web application I built using Laravel. I hope you like it.
+        </p>
+        <p>
+            <a class="btn btn-lg btn-success" href="#" role="button">Sign up now</a>
+        </p>
+    </div>
 @stop
