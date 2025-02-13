da8712d38b84bfa409ce23899b257c51352e8963
Lu Stormstout <lustormstout@gmail.com>
2025-02-13 21:35:45 +0900

    增加 CSRF 验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,6 +7,8 @@ use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use mysql_xdevapi\Exception;
 
 class UsersController extends Controller
 {
@@ -29,5 +31,18 @@ class UsersController extends Controller
     public function show(User $user): View|Factory|Application
     {
         return view('users.show', compact('user'));
+    }
+
+    /**
+     * @throws ValidationException
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|unique:users|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+        return;
     }
 }
